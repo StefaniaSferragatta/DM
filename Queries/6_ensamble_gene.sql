9c7b8fce1d02fb93e61784338cd4122851831d54
Stefania Sferragatta <sferragatta.stefania@gmail.com>
2021-03-30 21:59:15 +0200

    Delete Queries directory

--- Queries/6_ensamble_gene.sql
+++
@@ -1,4 +0,0 @@
-# Find ensembl gene id subjected to a specific type of alteration 
-SELECT EnsemblgeneID as ensemble_gene 
-from converter 
-where (Hugo_gene) in (SELECT Hugo_gene from oncokb_drug where alterations = 'Fusions');
