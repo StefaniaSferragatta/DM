6be72c84f72a2180ea7d4dc15e143fabef5d2346
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2021-04-01 22:15:12 +0200

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+rna = pd.read_csv('rna.csv', index_col=0)
+converter = pd.read_csv('converter_null.csv', names=['NCBI','Hugo'], index_col=1)
+l = list(set(converter.index).intersection(set(rna.index)))
+converter.loc[l]
+d = dict(zip(converter.index,converter['Hugo']))
+rna['Hugo'] = ''
+rna['Hugo'] = rna.index.map(d)
+rna = rna.reset_index()
+rna.to_csv('rna.csv',sep=' ', index=False)
+onco = pd.read_csv('onco_genes.csv', header=0,names=['Hugo_gene','x','Oncokbannotated','Isoncogene','Istumorsuppressorgene'])
+onco = onco.drop(['x'],axis=1)
+d2 = {'Yes':1,'No':0}
+onco['Oncokbannotated'] = onco['Oncokbannotated'].map(d2)
+onco['Isoncogene'] = onco['Isoncogene'].map(d2)
+onco['Istumorsuppressorgene'] = onco['Istumorsuppressorgene'].map(d2)
+rna.to_csv('onco.csv',sep=' ', index=False)
+drug = pd.read_csv('oncokb_drug.csv', header=0, names=['Hugo_gene','Alterations','Cancertypes','Drugs'])
+drug.to_csv('drug.csv',sep=' ', index=False)
