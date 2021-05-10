996b8ea46bb9e894423213b4f9e1ec1acf3f5567
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2021-05-10 22:14:19 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -17,3 +17,15 @@ onco['Istumorsuppressorgene'] = onco['Istumorsuppressorgene'].map(d2)
 rna.to_csv('onco.csv',sep=' ', index=False)
 drug = pd.read_csv('oncokb_drug.csv', header=0, names=['Hugo_gene','Alterations','Cancertypes','Drugs'])
 drug.to_csv('drug.csv',sep=' ', index=False)
+
+
+#--------------------------------------------------------------------------
+
+import pandas as pd
+rna = pd.read_csv('rna.csv',sep=',')
+threshold = rna.groupby('Hugo').mean()['FPKM'].to_dict()
+td_df = pd.DataFrame.from_dict(threshold,orient='index')
+new_df = pd.merge(td_df,rna,left_index=True,right_on='Hugo')
+filtered_df = new_df[new_df[0] < new_df['FPKM']]   
+filtered_df = filtered_df[['Sample','Cancer','Hugo']]
+filtered_df.to_csv('filtered.csv',sep=',',index=False)
